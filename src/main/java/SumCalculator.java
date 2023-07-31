fedae4b960409cafcdc1d30e6d6c64c6af9dc534
vlshutyak <vl.shutyak@gmail.com>
2023-07-31 15:52:09 +0300

    SumCalculator created.

--- src/main/java/SumCalculator.java
+++ src/main/java/SumCalculator.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class SumCalculator {
+public class SumCalculator {
+    int sum(int n){
+        if (n==0){
+            throw new IllegalArgumentException("Bad argument 0");
+        }
+        int sum=0;
+        for (int i = 1; i <=n ; i++) {
+            sum+=i;
+        }
+        return sum;
+    }
 }
