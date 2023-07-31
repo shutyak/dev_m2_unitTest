db4795b39c5e066f317e06164969c7e5c921479d
vlshutyak <vl.shutyak@gmail.com>
2023-07-31 15:49:42 +0300

    test created.

--- src/test/java/SumCalculatorTest.java
+++ src/test/java/SumCalculatorTest.java
@@ -1,4 +1,41 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SumCalculatorTest {
-  
+
+    @Test
+    void testSum1() {
+
+        //Given
+        final var sumCalculator = new SumCalculator();
+
+        //When
+        int result = sumCalculator.sum(1);
+
+        //Then
+        final int expected = 1;
+        assertEquals(result, expected);
+    }
+
+    @Test void testSum3() {
+
+        //Given
+        final var sumCalculator = new SumCalculator();
+
+        //When
+        int result = sumCalculator.sum(3);
+
+        //Then
+        final int expected = 6;
+        assertEquals(result, expected);
+    }
+    @Test void testSum0() {
+
+        //Given
+        final var sumCalculator = new SumCalculator();
+
+        //When & Then
+        assertThrows(IllegalArgumentException.class,()->sumCalculator.sum(0));
+    }
 }
